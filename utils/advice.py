03699ab66fc3bf1c11d26451f8c5c3687767273c
Vladimír Pokorný <vpokorny@fides.com>
2024-03-28 15:48:07 +0100

    checking if the advice is existing

--- utils/advice.py
+++ utils/advice.py
@@ -7,9 +7,30 @@ class Advice:
     self.advice_url = "https://api.adviceslip.com/advice"
 
   def get_random_advice(self):
-    response = requests.get(self.advice_url)
-    if response.status_code == 200:
+    response = requests.get(f"{self.advice_url}/{id}")
+    if response.status_code == 200 and self.is_advice_exist(id):
         advice = response.json()['slip']['advice']
         return advice
+    elif response.status_code == 200 and not self.is_advice_exist(id):
+        message = response.json()['message']['text']
+        return message
     else:
         print("Failed to retrieve advice")
+  
+  def get_advice_by_id(self, id):
+    response = requests.get(f"{self.advice_url}/{id}")
+    if response.status_code == 200 and self.is_advice_exist(id):
+        advice = response.json()['slip']['advice']
+        return advice
+    elif response.status_code == 200 and not self.is_advice_exist(id):
+        message = response.json()['message']['text']
+        return message
+    else:
+        print("Failed to retrieve advice")
+
+  def is_advice_exist(self, id):
+    response = requests.get(f"{self.advice_url}/{id}")
+    if response.status_code == 200 and response.json()['message']['type'] != 'error':
+        return True
+    else:
+        return False
